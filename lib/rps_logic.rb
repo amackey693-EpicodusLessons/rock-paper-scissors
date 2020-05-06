944b781f0c034b7b3ce8bf5bc677f86a07b7b08a
Allison Mackey <a.mackey693@gmail.com>
2020-05-06 10:26:38 -0700

    write & test spec #4, expect to fail

--- lib/rps_logic.rb
+++ lib/rps_logic.rb
@@ -7,13 +7,18 @@ class RPS
     @player2 = player2
   end
 
-  def wins?(player1, playere2) 
-    # conditions for player one winning 
+  def wins?(player1, player2) 
+    # conditions for player one winning // if results = false player 2 wins 
     if @player1 == "rock" && @player2 == "scissors" || @player1 == "paper" && @player2 == "rock" || @player1 == "scissors" && @player2 == "paper"
      results = true
     else 
      results = false    
     end
+
+    #conditions for tie game!
+    # if @player1 == "rock" && @player2 == "rock" || @player1 == "scissors" && @player2 == "scissors" || @player1 == "paper" && @player2 == "paper" 
+    #   results = "It's a tie game!"
+    #  end
     results
   end
 end
--- spec/rps_logic_spec.rb
+++ spec/rps_logic_spec.rb
@@ -14,4 +14,8 @@ describe ('RPS#') do
   game = RPS.new("rock", "paper")
   expect(game.wins?("rock", "paper")).to(eq(false))
  end 
+ it("returns 'It's a tie!' if player1 & player2 select the same option") do 
+  game = RPS.new("rock", "rock")
+  expect(game.wins?("rock", "rock")).to(eq("It's a tie game!"))
+ end 
 end 
