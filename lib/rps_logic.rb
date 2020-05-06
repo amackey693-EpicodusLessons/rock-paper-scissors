77385c02e804c004b46c7c12462be8ae984e74b7
Allison Mackey <a.mackey693@gmail.com>
2020-05-06 09:42:02 -0700

    add text to rps_logic.rb to pass test on spec #1, spec PASSES.

--- lib/rps_logic.rb
+++ lib/rps_logic.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 require('pry') 
 
+class RPS 
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def wins?(player1, player2) 
+    if @player1 == "rock" && @player2 == "scissors"
+      results = true
+    end
+    results
+  end
+end
--- spec/rps_logic_spec.rb
+++ spec/rps_logic_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
-require('rps')
+require('rps_logic')
 
 describe ('RPS#') do
  it("returns true if rock is the object and scissors is the argument") do 
-  game = RPS.new()
+  game = RPS.new("rock", "scissors")
   expect(game.wins?("rock", "scissors")).to(eq(true))
  end 
 end 
