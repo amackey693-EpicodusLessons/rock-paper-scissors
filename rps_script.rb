e6e1a89d956e0b3e19ab7b5e47fa3a4ed1f314a7
Allison Mackey <a.mackey693@gmail.com>
2020-05-06 13:39:59 -0700

    initialize object with two variables to keep score

--- rps_script.rb
+++ rps_script.rb
@@ -3,11 +3,18 @@ require('./lib/rps_logic')
 
 game = RPS.new()
 
-puts "Player One: Choose rock, paper or scissors!"
-player1 = gets.chomp
-puts "Player Two: Choose rock, paper or scissors!"
-player2 = gets.chomp
-puts game.wins?(player1, player2)
+def run_game
+  puts "Player One: Choose rock, paper or scissors!"
+  player1 = gets.chomp
+  puts "Player Two: Choose rock, paper or scissors!"
+  player2 = gets.chomp
+  puts game.wins?(player1, player2)
 
+  puts "Would you like to play again? "
+  answer = gets.chomp
+  if (answer == "y" || answer == "yes")
+    run_game()
+  end
+end
 
-
+run_game()
